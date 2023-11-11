7bb1f1fe0415361db50d8bb7a9951de21ce85106
Jeftbook <109643021+Jeftbook@users.noreply.github.com>
2023-11-11 15:57:25 -0300

    Traer datos de firebase

--- src/assets/components/UserPanel.jsx
+++ src/assets/components/UserPanel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,9 +8,9 @@ import { db } from "../../firebase/config.js";
 
 export const UserPanel = () => {
   const [completo, setCompleto] = useState(false);
-  const [name, setName] = useState("Martu");
-  const [surname, setSurname] = useState("Juri");
-  const [email, setEmail] = useState("martijuri11@gmail.com");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
   const [age, setAge] = useState("");
   const [dni, setDni] = useState("");
   const [ambit, setAmbit] = useState("");
@@ -18,10 +18,29 @@ export const UserPanel = () => {
   const [workedAs, setWorkedAs] = useState("");
   const [biography, setBiography] = useState("");
 
-  const docRef = doc(db, "users", userId)
+  readUser();
+  // Traer datos del usuario 
+  async function readUser() {
+    const docRef = doc(db, "users", 'p2yKmutiP8XjecN0HfManzVGEl32');
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setName(docSnap.data().nombre);
+      setSurname(docSnap.data().apellido);
+      setEmail(docSnap.data().email);
+      setAge(docSnap.data().edad);
+      setDni(docSnap.data().dni);
+      setAmbit(docSnap.data().ambito);
+      setDiscipline(docSnap.data().disciplina);
+      setWorkedAs(docSnap.data().trabajo_de);
+      setBiography(docSnap.data().biografia);
+    } else {
+      console.log("No such document!");
+    }
+  }
 
 
-  const isCompleto = () =>  {
+  const isCompleto = () => {
     return (
       age.trim() !== "" &&
       dni.trim() !== "" &&
@@ -48,7 +67,7 @@ export const UserPanel = () => {
       // Los campos están completos, puedes realizar las acciones necesarias aquí
       setCompleto(true);
       setIsEditing(false);
-      updateUserGen(id, name, surname, email, age, dni, ambit, discipline, workedAs, biography);
+      // updateUserGen(id, name, surname, email, age, dni, ambit, discipline, workedAs, biography);
     }
   };
 
@@ -74,67 +93,67 @@ export const UserPanel = () => {
     "Otro",
   ];
 
- 
+
 
   return (
     <>
       <div>
-      <UserHeader />
+        <UserHeader />
         {!completo && (
           <div className="container-form">
             <h2 >Complete los datos de su perfil</h2>
-            
+
             <div className="field-group">
 
-            <div className="input-container">
-            <label  className="categoria" htmlFor="">Nombre </label>
-            <input className="input"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+              <div className="input-container">
+                <label className="categoria" htmlFor="">Nombre </label>
+                <input className="input"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
 
-            </div> 
-            <div className="input-container">
-            <label className="categoria"htmlFor="">Apellido </label>
-            <input className="input"
-              type="text"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
-            />
-            </div>
+              </div>
+              <div className="input-container">
+                <label className="categoria" htmlFor="">Apellido </label>
+                <input className="input"
+                  type="text"
+                  value={surname}
+                  onChange={(e) => setSurname(e.target.value)}
+                />
+              </div>
             </div>
 
             <div className="input-container">
-            <label className="categoria" htmlFor="">Correo Electrónico </label>
-            <input className="input"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+              <label className="categoria" htmlFor="">Correo Electrónico </label>
+              <input className="input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="field-group">
-            
-            <div className="input-container">
-            <label className="categoria"htmlFor="">DNI </label>
-            <input className="input"
-              onChange={(e) => setDni(e.target.value)}
-              type="text"
-              placeholder="DNI"
-            />
-            </div>
-            <div className="input-container">
-            <label className="categoria"htmlFor="">Edad </label>
-            <input className="input"
-              onChange={(e) => setAge(e.target.value)}
-              type="number"
-              placeholder="Edad"
-            />
-            </div>
+
+              <div className="input-container">
+                <label className="categoria" htmlFor="">DNI </label>
+                <input className="input"
+                  onChange={(e) => setDni(e.target.value)}
+                  type="text"
+                  placeholder="DNI"
+                />
+              </div>
+              <div className="input-container">
+                <label className="categoria" htmlFor="">Edad </label>
+                <input className="input"
+                  onChange={(e) => setAge(e.target.value)}
+                  type="number"
+                  placeholder="Edad"
+                />
+              </div>
             </div>
 
             <div className="input-container">
-            <label className="categoria"htmlFor="">Ámbito </label>
+              <label className="categoria" htmlFor="">Ámbito </label>
               <select className="input"
                 value={ambit}
                 onChange={(e) => setAmbit(e.target.value)}
@@ -147,24 +166,24 @@ export const UserPanel = () => {
                 ))}
               </select>
             </div>
-            
+
             <div className="input-container">
-            <label className="categoria"htmlFor="">Disciplina </label>
-            <select className="input"
-              value={discipline}
-              onChange={(e) => setDiscipline(e.target.value)}
-            >
-              <option value="">Selecciona una disciplina</option>
-              {disciplinaOptions.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
+              <label className="categoria" htmlFor="">Disciplina </label>
+              <select className="input"
+                value={discipline}
+                onChange={(e) => setDiscipline(e.target.value)}
+              >
+                <option value="">Selecciona una disciplina</option>
+                {disciplinaOptions.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
             </div>
-            
+
             <div className="input-container">
-            <label className="categoria"htmlFor="">Trabajó de </label>
+              <label className="categoria" htmlFor="">Trabajó de </label>
               <select className="input"
                 value={workedAs}
                 onChange={(e) => setWorkedAs(e.target.value)}
@@ -177,20 +196,20 @@ export const UserPanel = () => {
                 ))}
               </select>
             </div>
-            
+
             <div className="input-container">
-            <label className="categoria" htmlFor="">Biografía </label>
-            <textarea className="input"
-              onChange={(e) => setBiography(e.target.value)}
-              name=""
-              id=""
-              cols="40"
-              rows="5"
-            ></textarea>
+              <label className="categoria" htmlFor="">Biografía </label>
+              <textarea className="input"
+                onChange={(e) => setBiography(e.target.value)}
+                name=""
+                id=""
+                cols="40"
+                rows="5"
+              ></textarea>
             </div>
-            
+
             <button className="button" onClick={handleSave}>Guardar perfil</button>
-          </div>   
+          </div>
         )}
       </div>
       <ToastContainer />
